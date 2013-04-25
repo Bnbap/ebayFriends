2ff7019bab9bbeef1706a18cc1482d9b07409410
Hank Zhang <wtksyjpy@gmail.com>
2013-04-25 17:10:31 +0800

    notification change

--- src/notification/client/NotificationAccess.java
+++ src/notification/client/NotificationAccess.java
@@ -1,11 +1,17 @@
 package notification.client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import com.ebay.ebayfriend.R;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 public class NotificationAccess {
 	private Activity callBackActivity;
@@ -44,6 +50,26 @@ public class NotificationAccess {
 	public void notifyUser() {
 		try {
 			socket = new Socket("10.0.2.2", 1234);
+			// 向服务器发送消息
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new OutputStreamWriter(socket.getOutputStream())), true);
+			out.println("haofohwohiowfhoiohwhfsjafhkjs-=-=-=-");
+
+			// 接收来自服务器的消息
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			String msg = br.readLine();
+
+			if (msg != null) {
+				Log.e(msg, msg);
+			} else {
+				Log.e("数据错误!", "数据错误!");
+			}
+			// 关闭流
+			out.close();
+			br.close();
+			// 关闭Socket
+			socket.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
