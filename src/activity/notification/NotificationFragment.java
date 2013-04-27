31514958d7a5927a871055afa06ed769b54571df
Hank Zhang <wtksyjpy@gmail.com>
2013-04-27 16:50:34 +0800

    notification settings

--- src/activity/notification/NotificationFragment.java
+++ src/activity/notification/NotificationFragment.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import activity.newsfeed.PullAndLoadListView;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +20,8 @@ import com.ebay.ebayfriend.R;
 
 public class NotificationFragment extends ListFragment {
 	private List<Map<String, Object>> mData;
+	private NotificationAdapter na;
+	private PullAndLoadListView lv;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,10 +38,12 @@ public class NotificationFragment extends ListFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setListAdapter(new SimpleAdapter(getActivity(), getData(),
+		mData = getData();
+		na = new NotificationAdapter(getActivity(), mData,
 				R.layout.comments_list,
 				new String[] { "title", "info", "img" }, new int[] {
-						R.id.title, R.id.info, R.id.img }));
+						R.id.title, R.id.info, R.id.img });
+		setListAdapter(na);
 	}
 
 	public void onListItemClick(ListView parent, View v, int position, long id) {
@@ -49,32 +53,6 @@ public class NotificationFragment extends ListFragment {
 						+ (String) mData.get(position).get("title"),
 				Toast.LENGTH_SHORT).show();
 	}
-
-	/*
-	 * // ListView ��������������������
-	 * 
-	 * @Override protected void onListItemClick(ListView l, View v, int
-	 * position, long id) {
-	 * 
-	 * Log.v("MyListView4-click", (String) mData.get(position).get("title")); }
-	 */
-
-	// /**
-	// * listview��������������������
-	// */
-	// public void showInfo() {
-	// new AlertDialog.Builder(this.getActivity())
-	// .setTitle("����listview")
-	// .setMessage("����...")
-	// .setPositiveButton("����",
-	// new DialogInterface.OnClickListener() {
-	// @Override
-	// public void onClick(DialogInterface dialog,
-	// int which) {
-	// }
-	// }).show();
-	//
-	// }
 
 	private List<Map<String, Object>> getData() {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
--- src/activity/setting/SettingFragment.java
+++ src/activity/setting/SettingFragment.java
@@ -2,6 +2,7 @@ package activity.setting;
 
 import util.LoginUtil;
 import activity.login.LoginActivity;
+import activity.notification.NotificationSettingsActivity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,29 +18,31 @@ import android.widget.Toast;
 
 import com.ebay.ebayfriend.R;
 
-public class SettingFragment extends Fragment implements OnItemClickListener{
+public class SettingFragment extends Fragment implements OnItemClickListener {
 	private ListView lv;
-	String[] settingItems = {"Log out","clean history"};
+	String[] settingItems = { "Log out", "clean history",
+			"Notification Settings" };
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.setting, container, false);
-		TextView windowTitleView = (TextView)getActivity().findViewById(R.id.window_title);
+		TextView windowTitleView = (TextView) getActivity().findViewById(
+				R.id.window_title);
 		windowTitleView.setText("Setting");
 
-		lv = (ListView)view.findViewById(R.id.setting_listview);
-		lv.setAdapter(new ArrayAdapter<String>(this.getActivity(), R.layout.item,
-				R.id.tv_item, settingItems));
+		lv = (ListView) view.findViewById(R.id.setting_listview);
+		lv.setAdapter(new ArrayAdapter<String>(this.getActivity(),
+				R.layout.item, R.id.tv_item, settingItems));
 		lv.setOnItemClickListener(this);
 		return view;
 	}
-	
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
-	long id) {
+			long id) {
 		// TODO Auto-generated method stub
-		switch(position){
+		switch (position) {
 		case 0:
 			LoginUtil.removeSession();
 			LoginUtil.deleteFile();
@@ -50,9 +53,15 @@ public class SettingFragment extends Fragment implements OnItemClickListener{
 			break;
 		case 1:
 			LoginUtil.deleteFile();
-			Toast.makeText(getActivity(), "clean history successfully", Toast.LENGTH_SHORT).show();
+			Toast.makeText(getActivity(), "clean history successfully",
+					Toast.LENGTH_SHORT).show();
 			break;
-			
+		case 2:
+			Intent intent2 = new Intent();
+			intent2.setClass(getActivity(), NotificationSettingsActivity.class);
+			getActivity().startActivity(intent2);
+			getActivity().finish();
+
 		}
 	}
 }
