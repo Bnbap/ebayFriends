3a7bd7052935ff0754f1ff7cc9639c36de22565b
njuyyz <njuyyzh@gmail.com>
2013-04-30 12:06:55 +0800

    com

--- src/activity/post/AttachItemFragment.java
+++ src/activity/post/AttachItemFragment.java
@@ -1,44 +1,112 @@
 package activity.post;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import util.PostNewUtil;
+
+import activity.about.AboutFragment;
+import activity.newsfeed.NewsFeedFragment;
+import activity.notification.NotificationFragment;
+import activity.profile.ProfileFragment;
+import activity.setting.SettingFragment;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import bean.PurchasedItem;
+import android.widget.ListView;
 
 import com.ebay.ebayfriend.R;
 
 public class AttachItemFragment extends Fragment {
 
+	private ArrayList<HashMap<String, Object>> itemList;
+	private ListView lv;
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
-	        Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.attachitem,container,false);
-		TextView windowTitleView = (TextView)view.findViewById(R.id.window_title);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.attachitem, container, false);
+		TextView windowTitleView = (TextView) getActivity()
+				.findViewById(R.id.window_title);
 		windowTitleView.setText("Post");
+
+		
+		Thread getItemListThread = new GetItemListThread();
+		getItemListThread.start();
+		try {
+			getItemListThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		lv = (ListView) view.findViewById(R.id.purchased_list);
+
+		SimpleAdapter listItemAdapter = new SimpleAdapter(getActivity(),
+				itemList, R.layout.puchased_item,
+				new String[] { "date", "name" }, new int[] {
+						R.id.purchasedItem_date, R.id.purchasedItem_name });
+		lv.setAdapter(listItemAdapter);
+		lv.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				String name = ((TextView)view.findViewById(R.id.purchasedItem_name)).getText().toString();
+				String date =((TextView)view.findViewById(R.id.purchasedItem_date)).getText().toString();
+				switchToPicProcessFragment(new PurchasedItem(name,date));
+								
+			}
+			
+		});
 		return view;
 	}
-	
+	private void switchToPicProcessFragment(PurchasedItem pi){
+		PicProcessFragment fragment = new PicProcessFragment();
+		fragment.setPurchasedItem(pi);
+		FragmentTransaction transaction = getFragmentManager()
+				.beginTransaction();
+		transaction.replace(R.id.content, fragment);
+		transaction.addToBackStack(null);
+		transaction.commit();
+	}
+
 	/**
-	 * get the user's purchase records 
+	 * get the user's purchase records
 	 * 
-	 * @param UserId the user who concerns
+	 * @param UserId
+	 *            the user who concerns
 	 * @return PurchasedItems
 	 */
-	private PurchasedItem[] getBoughtRecords(long UserId){
+	private PurchasedItem[] getBoughtRecords(long UserId) {
 		return null;
 	}
-	
-	class SelectItem implements OnClickListener{
+
+	class SelectItem implements OnClickListener {
 
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
+	}
+	
+	class GetItemListThread extends Thread{
+		@Override
+		public void run(){
+			itemList = PostNewUtil.getItemList();
+		}
 	}
 }
--- src/util/PostNewUtil.java
+++ src/util/PostNewUtil.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -11,11 +12,11 @@ import bean.PurchasedItem;
 
 public class PostNewUtil {
 
-	private static List<PurchasedItem> itemList = new ArrayList<PurchasedItem>();
+	private static ArrayList<HashMap<String,Object>> itemList = new ArrayList<HashMap<String,Object>>();
 
-	public List<PurchasedItem> getItemList() {
+	public static ArrayList<HashMap<String,Object>> getItemList() {
 
-		GetRequest gr = new GetRequest("URL");
+		GetRequest gr = new GetRequest("http://192.168.47.19:8080/users/getGoodsList");
 		JSONArray jsonArray = null;
 		try {
 			jsonArray = new JSONArray(gr.getContent());
@@ -27,10 +28,17 @@ public class PostNewUtil {
 				JSONObject jObject = (JSONObject) jsonArray.get(i);
 				String name = null;
 				String url = null;
+				String date =null;
 				name = jObject.getString("name");
 				url = jObject.getString("url");
-				PurchasedItem pi = new PurchasedItem(name,url);
-				itemList.add(pi);
+				date = jObject.getString("date");
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("name", name);
+				map.put("url", url);
+				map.put("date", date);
+				
+//				PurchasedItem pi = new PurchasedItem(name,url);
+				itemList.add(map);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
