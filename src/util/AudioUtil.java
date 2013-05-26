b98c6cfdb91ecd27a851df9323f46ae5cc90aa17
RyanYe <njuyyzh@gmail.com>
2013-05-26 15:58:05 +0800

    change voice process gif

--- src/util/AudioUtil.java
+++ src/util/AudioUtil.java
@@ -32,12 +32,9 @@ public class AudioUtil {
 	public void recordVoice(String fileName) {
 		mFileName = fileName;
 		mRecorder = new MediaRecorder();
-		// 设置音源为Micphone
 		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		// 设置封装格式
 		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 		mRecorder.setOutputFile(mFileName);
-		// 设置编码格式
 		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
 		try {
@@ -59,29 +56,9 @@ public class AudioUtil {
 	}
 
 	public void playVoice() {
-//		try {
-//			int sessionId = mPlayer.getAudioSessionId();
-//			Equalizer ae = new Equalizer(0,sessionId);
-//			ae.setEnabled(true);
-//			short bands = ae.getNumberOfBands();
-//			for(short i=0;i<bands;i++){
-//				short range[] = ae.getBandLevelRange();
-//				
-//				ae.setBandLevel(i, (short)i>=bands/2?range[0]:range[1]);
-//			}
-//			mPlayer.attachAuxEffect(ae.getId());
-			
-			
-//			mPlayer.setDataSource(mFileName);
-//			mPlayer.prepare();
-//			mPlayer.start();
-//			
 			int soundId = sp.load(mFileName, 1);
 			sp.play(soundId, 1, 1, 1, 0,  frequency);
 			Log.e(LOG_TAG, "play success");
-//		} catch (IOException e) {
-//			Log.e(LOG_TAG, "prepare() failed");
-//		}
 	}
 	public void changeFrequencyAndPlay(float frequency,Handler h){
 		this.frequency=frequency;
